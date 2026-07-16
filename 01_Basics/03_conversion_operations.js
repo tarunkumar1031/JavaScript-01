81df3ec78c2ec42350e95afe72e8319af049dff1
TARUN KUMAR <tarunkumar954865@gmail.com>
2026-07-16 19:30:29 +0000

    conversion of data types

---
+++ 01_Basics/03_conversion_operations.js
@@ -0,0 +1,26 @@
+// let score = 33
+// console.log(typeof score);
+
+// let score = "33abv"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  // NaN
+
+
+// "33" => 33
+// "44abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "tarun" => false
+
