7d23f174cfecb180758bac2a5dce7c265ae19d36
TARUN KUMAR <tarunkumar954865@gmail.com>
2026-07-18 06:33:11 +0000

    datatypes summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,43 @@
+// primitive
+
+//  7 types : string , number , boolean, null ,undefined , symbol , BigInt
+
+// const score = 22;
+
+//  jS is dynamically typed bcoz data types automatciavlly defined 
+
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('232')
+const anotherid = Symbol('232')
+
+// console.log(id === anotherid);
+
+// const bigNumber = 34388448223222n
+
+//  Reference ( Non Primitive)
+
+//  Array , object , functions
+
+
+const heros = ["tarun ", "taur","hsid"];
+
+console.log(heros);
+
+let myobj = { 
+    name: "tarun",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello bhai");
+    
+
+}
