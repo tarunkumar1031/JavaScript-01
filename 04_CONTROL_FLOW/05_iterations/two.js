f68840dc13931529af5fd888961409bf6f3adb00
TARUN KUMAR <tarunkumar954865@gmail.com>
2026-08-15 09:51:04 +0000

    while , dowhile

---
+++ 04_CONTROL_FLOW/05_iterations/two.js
@@ -0,0 +1,43 @@
+//  while
+
+// while (condition) {
+    
+// }
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+//     index++
+    
+    
+// }
+
+// let myArray = ["falsu", "superman" , "huoa"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+//  do while loop
+
+// do {
+    
+// } while (condition);
+
+
+// let score = 1
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while (score <= 10);
+
+
+// let score = 11
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while (score <= 10);
